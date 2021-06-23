c412dc5fdcd79f921f991b29b50db68c01cc4d04
John Nguyen <jvn1567@gmail.com>
2021-06-22 19:16:09 -0700

    initial commit

---
+++ src/sudokuSolver/Launcher.java
@@ -0,0 +1,14 @@
+package sudokuSolver;
+
+import javafx.application.Application;
+
+/**
+ *
+ * @author John
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        Application.launch(SudokuSolver.class);
+    }
+}
